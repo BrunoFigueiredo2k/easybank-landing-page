d8d5ef4171cdd0619f6256f38222189bb8ee3c73
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2020-12-09 18:14:08 +0100

    mobile fixed

--- js/index.js
+++ js/index.js
@@ -5,8 +5,6 @@ const mobileMenu = document.querySelector('#mobile-menu-li');
 
 const mobileBtn = document.querySelector('#mobile-btn');
 var btnSrc = document.getElementById('mobile-btn').src;
-console.log(btnSrc);
-var mobileMenuActive = false;
 
 // Click listener hamburger menu btn
 // TODO: fix
@@ -14,10 +12,19 @@ mobileBtn.addEventListener('click', () => {
     if (btnSrc == HAMBURGER_ICON_SRC){
         btnSrc = CLOSE_ICON_SRC;
         mobileMenu.style.display = 'block';
-        console.log(btnSrc)
+
+        // Onclick outside hide again
+        event.stopPropagation();
+        window.onclick = function(e) {
+            if(e.target != mobileMenu) {
+                btnSrc = HAMBURGER_ICON_SRC;
+                mobileMenu.style.display = 'none';
+            } else {
+                mobileMenu.style.display = 'block';
+            }
+        }    
     } else {
         btnSrc = HAMBURGER_ICON_SRC;
         mobileMenu.style.display = 'none';
-        console.log(btnSrc)
     }
 });
